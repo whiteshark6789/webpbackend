b9043d32714d20084c659c87e555f5971dc46903
Ani B <anirudh.020406@gmail.com>
2026-03-30 15:41:36 +0530

    Fix: case-sensitive Order model path for Linux/Render

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Order = require("../models/Order");
+const Order = require("../models/order");
 const Product = require("../models/Product");
 const Cart = require("../models/Cart");
 
